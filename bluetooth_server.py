e62c92d4ee920e91bbf90a3135297ceb1ea47c64
Muhammed Guler <mhmmdglr92@gmail.com>
2023-09-11 16:58:23 +0300

    Revert "send ip düzelnlendi"
    This reverts commit a9b3dd3c3451dcab658de7634bf948ba9d294ea3.

--- bluetooth_server.py
+++ bluetooth_server.py
@@ -3,7 +3,6 @@ import glob
 import time
 from bluetooth import *
 import threading
-import netifaces
 
 class BluetoothServer:
     def __init__(self) -> None:
@@ -15,7 +14,7 @@ class BluetoothServer:
         self.wifiname = None
         self.wifipassword = None
         self.ip = None
-        self.WifiConnection = False
+        
 
 
 
@@ -41,18 +40,18 @@ class BluetoothServer:
                             )
         
     def send_ip(self):
-        self.ip = None
-        self.timeout = time.time()
-        while((self.ip==None) and (self.WifiConnection) ):
-            try:
-                time.sleep(0.01)
-                if((time.time()-self.timeout)>300):
-                    self.WifiConnection = False  
-                    break
-                self.ip = netifaces.ifaddresses('wlan0')[netifaces.AF_INET][0]['addr']
-            except:
-                pass
-        print(self.ip)  # should print "192.168.100.37"
+        time.sleep(15)
+        self.ip = "Alınamadı"
+        os.system("sudo rm -r ifconfig.txt")
+        os.system("ifconfig > ifconfig.txt")
+        file = open("/home/pi/Desktop/ifconfig.txt","r")
+        lines = file.readlines()
+        for line in lines:
+            if "255.255.255" in line:
+                lineArray = line.split("inet")
+                lineArray = lineArray[1].split(" ")
+                self.ip = lineArray[1]
+                print(self.ip)
         self.client_sock.send(self.ip.encode())
         
     def waiting_connection(self):
@@ -73,8 +72,6 @@ class BluetoothServer:
                 print(data)
                 self.wifiname = data[0]
                 self.wifipassword = data[1]
-                self.WifiConnection = False
-                self.startTime = time.time()
                 #os.system("sudo rm -r /etc/wpa_supplicant/wpa_supplicant.conf")
                 os.system("sudo touch /etc/wpa_supplicant/wpa_supplicant.conf")
                 wpa = """ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
@@ -92,9 +89,6 @@ network={{
                 os.system("wpa_cli -i wlan0 reconfigure")
                 print("wifi name:",self.wifiname)
                 print("wifi password:",self.wifipassword)
-                if((time.time()-self.startTime)<0.05):
-                    time.sleep((0.05-(time.time()-self.startTime)))
-                self.WifiConnection = True
                 threading.Thread(target=self.send_ip,daemon=True).start()
             except IOError:
                 print("Connection disconnected!")
